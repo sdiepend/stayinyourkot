3be646d2f2e32a05c75651d59e1c72206b0c09eb
Stijn Diependaele <sdiepend@gmail.com>
2020-03-28 16:44:44 +0100

    Add growth data

--- viz/app.py
+++ viz/app.py
@@ -20,28 +20,33 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets, server=serv
 #fig_cases_death = go.Figure(data=[go.Scatter(x=df_cases_death.iloc[0][4:], y=df_cases_death[df_cases_death['Country/Region'] == 'Belgium'].iloc[0][4:])])
 
 # Read the data from github
-df_cases_belgium = pd.read_csv("https://raw.githubusercontent.com/sdiepend/stayinyourkot/master/COVID19_Belgium_cases.csv")
+df_cases_belgium = pd.read_csv("https://raw.githubusercontent.com/sdiepend/stayinyourkot/master/data/COVID19_Belgium_cases.csv")
+df_growth_belgium = pd.read_csv("https://raw.githubusercontent.com/sdiepend/stayinyourkot/master/data/COVID19_Belgium_growth.csv")
 
 # Select only the severe cases columns
-df_belgium_severe = df_cases_belgium[['date', 'daily_infected', 'death', 'icu', 'capacity_icu', 'hospitalized']][13:]
+df_belgium_severe = df_cases_belgium[['date', 'daily_infected', 'daily_deceased', 'deceased', 'icu', 'capacity_icu', 'hospitalized']][13:]
+df_growth_belgium_severe = df_growth_belgium[['date', 'daily_infected', 'daily_deceased', 'deceased', 'icu', 'capacity_icu', 'hospitalized']][13:]
 
 # Correctly format icu and hospitalized
-df_belgium_severe[['icu', 'hospitalized']] = df_belgium_severe[['icu', 'hospitalized']].astype(int)
+df_belgium_severe[['daily_deceased', 'icu', 'hospitalized']] = df_belgium_severe[['daily_deceased', 'icu', 'hospitalized']].astype(int)
 
 # create a table figure (ff = figure factory)
 table_severe = ff.create_table(df_belgium_severe)
+#table_severe.layout = {'title': 'Data'}
 
 # create line chart for the severe cases
 fig_severe = go.Figure()
 fig_severe.layout = {'title': 'Severe Cases'}
-fig_severe.add_trace(go.Scatter(x=df_belgium_severe['date'], y=df_belgium_severe['death'], name='deaths', line={'color': 'black'}))
+fig_severe.add_trace(go.Scatter(x=df_belgium_severe['date'], y=df_belgium_severe['deceased'], name='deaths', line={'color': 'black'}))
 fig_severe.add_trace(go.Scatter(x=df_belgium_severe['date'], y=df_belgium_severe['icu'], name='ICU', line={'color' : 'tomato'}))
 fig_severe.add_trace(go.Scatter(x=df_belgium_severe['date'], y=df_belgium_severe['hospitalized'], name='hospitalized', line={'color': 'purple'}))
 fig_severe.add_trace(go.Scatter(x=df_belgium_severe['date'], y=df_belgium_severe['capacity_icu'], name='ICU Capacity', line={'color': 'tomato', 'dash': 'dashdot'}))
 
 fig_bar_infected_daily = go.Figure()
-fig_bar_infected_daily.layout={'title': 'Daily new infections'}
-fig_bar_infected_daily.add_trace(go.Bar(x=df_belgium_severe['date'], y=df_belgium_severe['daily_infected']))
+fig_bar_infected_daily.layout={'title': 'Daily new infections and deaths'}
+fig_bar_infected_daily.add_trace(go.Bar(x=df_belgium_severe['date'], y=df_belgium_severe['daily_infected'], name='New Infections'))
+fig_bar_infected_daily.add_trace(go.Bar(x=df_belgium_severe['date'], y=df_belgium_severe['daily_deceased'], name='New Deaths'))
+
 
 app.layout = html.Div(style={}, children=[
     html.H1(children='COVID19 Dashboard',
